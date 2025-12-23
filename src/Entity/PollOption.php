ffde04253ae3e46f78cbf736c383dab843e9ff7a
Tomas Schmidt <t.schmidt@codezilla.consulting>
2025-12-23 14:35:32 +0100

    add poll finish and winning option determination

---
+++ src/Entity/PollOption.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PollOptionRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PollOptionRepository::class)]
+class PollOption
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $votes = null;
+
+    #[ORM\ManyToOne(inversedBy: 'options')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Poll $poll = null;
+
+    /**
+     * @var Collection<int, User>
+     */
+    #[ORM\ManyToMany(targetEntity: User::class, inversedBy: 'pollOptions', cascade: ['persist'], fetch: 'EAGER')]
+    private Collection $users;
+
+    #[ORM\Column]
+    private ?bool $winner = null;
+
+    public function __construct()
+    {
+        $this->users = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getVotes(): ?int
+    {
+        return $this->votes;
+    }
+
+    public function setVotes(int $votes): static
+    {
+        $this->votes = $votes;
+
+        return $this;
+    }
+
+    public function getPoll(): ?Poll
+    {
+        return $this->poll;
+    }
+
+    public function setPoll(?Poll $poll): static
+    {
+        $this->poll = $poll;
+
+        return $this;
+    }
+
+    public function getUsers(): ?Collection
+    {
+        return $this->users;
+    }
+
+    public function setUsers(?Collection $users): static
+    {
+        $this->users = $users;
+
+        return $this;
+    }
+
+    public function addUser(User $user): static
+    {
+        if (!$this->users->contains($user)) {
+            $this->users->add($user);
+        }
+
+        return $this;
+    }
+
+    public function removeUser(User $user): static
+    {
+        $this->users->removeElement($user);
+
+        return $this;
+    }
+
+    public function isWinner(): ?bool
+    {
+        return $this->winner;
+    }
+
+    public function setWinner(bool $winner): static
+    {
+        $this->winner = $winner;
+
+        return $this;
+    }
+}
